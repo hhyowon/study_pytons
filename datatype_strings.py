18e22d9368fbbcff02c974b5d0437d89d6b342a3
hhyowon <hyowon0605@gmail.com>
2023-08-01 17:55:46 +0900

    hfdh

--- datatype_strings.py
+++ datatype_strings.py
@@ -1,10 +1,46 @@
-str = "Yojulab !"
-len(str)
+string = "Yojulab !"
+len(string)
 pass
 
-# len(str)
+len(string)
 # 9
-# "ju" in str
+
+# condition
+"ju" in string
 # True
-# "Not" not in str
+"Not" in string
+# False
+"Not" not in string
 # True
+
+# https://www.w3schools.com/python/python_stringings_slicing.asp
+#slicing
+
+string[3]
+# 'u'
+string[3:6]
+# 'ula'
+string[3:]
+# 'ulab !'
+string[:-2]
+# 'Yojulab'
+
+pass 
+
+age = 36
+txt = "My name is John, I am " + str(age)
+print(txt)
+
+
+quantity = 3  # index 0
+itemno = 567  # index 1
+price = 49.95  # index 2 
+myorder = "I want {0} pieces of item {1} for {2} dollars.I have just{2}."
+
+# myorder.format(quantity,itemno,price)
+# 'I want 3 pieces of item 567 for 49.95 dollars.I have just49.95.'
+
+pass
+
+
+
