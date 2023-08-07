572a8b92a33d3e9634d78db88b168967f9ad10b2
hhyowon <hyowon0605@gmail.com>
2023-08-07 10:19:07 +0900

    fghfgh

--- datatype_datetimes.py
+++ datatype_datetimes.py
@@ -6,5 +6,18 @@ today = datetime.datetime.now()
 datetime.datetime(2023, 8, 4, 18, 14, 5, 604539)
 type(today)
 # <class 'datetime.datetime'>
-yesterday = datetime.datetime(2023, 8, 3)
+someday = datetime.datetime(2023, 8, 3)
+
+someday -today
+# datetime.timedelta(days=-5, seconds=50505, microseconds=940281)
+datetime.datetime(2012,2,2)
+# datetime.datetime(2012, 2, 2, 0, 0)
+50578 /(24*60*60)
+# 0.5853935185185185
+somedelta = someday -today
+type(somedelta)
+# <class 'datetime.timedelta'>
+
+somedelta.days
+# -5
 pass
